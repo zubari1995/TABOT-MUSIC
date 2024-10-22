802c002476c3085e46962500b7d107ba0275a996
zubari1995 <166775989+zubari1995@users.noreply.github.com>
2024-10-22 23:44:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   voteManager: {
     status: false,
-    api_key: "",
+    api_key: "AIzaSyAvTqhvJmb2rBnVCcuJi3iuH3qyZPW3fNE",
     vote_commands: ["back", "channel", "clear", "dj", "filter", "loop", "nowplaying", "pause", "playnormal", "playlist", "queue", "resume", "save", "play", "skip", "stop", "time", "volume"],
     vote_url: "",
   },
