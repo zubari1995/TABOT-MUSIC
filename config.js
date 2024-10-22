3cfdac4e9c45268f3f8b73644bb4685c5792ce4d
zubari1995 <166775989+zubari1995@users.noreply.github.com>
2024-10-22 23:36:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["867646365056237619", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
